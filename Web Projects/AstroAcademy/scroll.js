24215d7eabe6fe2adde3de62633e8f3b36594dd8
MathmatheticGuy <nhatthanhdinh482@gmail.com>
2023-11-03 21:45:53 +0700

    scroll btn fix

--- Web Projects/AstroAcademy/scroll.js
+++ Web Projects/AstroAcademy/scroll.js
@@ -1,27 +1,25 @@
-// Get the button
+//Get the button
 let my_button = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = () => {
-    scrollFunction();
+window.onscroll = function () {
+  scrollFunction();
 };
 
-
-let scrollFunction = () => {
-    if (
-        document.body.scrollTop > 30 ||
-        document.documentElement.scrollTop > 30
-    ) {
-        my_button.style.display = "block";
-    } else{
-        my_button.style.display = "none"; 
-    }
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 5 ||
+    document.documentElement.scrollTop > 5
+  ) {
+    my_button.style.display = "block";
+  } else {
+    my_button.style.display = "none";
+  }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 my_button.addEventListener("click", backToTop);
 
 function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
